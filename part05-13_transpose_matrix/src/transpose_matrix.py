cb6dea861dfd119e28f77d6ff0ac95c12ed4e7ba
ameliacitradewi <ameliacitradewi@gmail.com>
2024-07-24 16:39:25 +0700

    commit

--- part05-13_transpose_matrix/src/transpose_matrix.py
+++ part05-13_transpose_matrix/src/transpose_matrix.py
@@ -1,1 +1,7 @@
 # Write your solution here
+def transpose(matrix: list):
+  n = len(matrix)
+  for i in range(n):
+      for j in range(i + 1, n):
+          # Swap elements across the diagonal
+          matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
