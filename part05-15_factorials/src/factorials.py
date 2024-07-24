1c1e26e2bf88d2c295770aa0ffe3de51dc5dcf75
ameliacitradewi <ameliacitradewi@gmail.com>
2024-07-24 21:40:11 +0700

    commit

--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def factorials(n: int) -> dict:
+  def factorial(num: int) -> int:
+      if num == 0 or num == 1:
+          return 1
+      result = 1
+      for i in range(2, num + 1):
+          result *= i
+      return result
+  
+  factorial_dict = {}
+  for i in range(1, n + 1):
+      factorial_dict[i] = factorial(i)
+  
+  return factorial_dict
--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def histogram(s: str):
+    dicts = {}
+    for char in s:
+        if char in dicts:
+            dicts[char] += 1
+        else:
+            dicts[char] = 1
+
+    for letter, count in sorted(dicts.items()):
+        print(f"{letter} {'*' * count}")
