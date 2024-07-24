9a2b84181e435c019fd9c9afada768104dac57df
ameliacitradewi <ameliacitradewi@gmail.com>
2024-07-24 22:20:29 +0700

    commit

--- part05-17_phone_book_v1/src/phone_book_v1.py
+++ part05-17_phone_book_v1/src/phone_book_v1.py
@@ -1,1 +1,19 @@
 # Write your solution here
+phone = {}
+
+while True:
+    command = (input("command (1 search, 2 add, 3 quit): "))
+    if command == "1":
+        name = input("name: ")
+        if name in phone:
+            print(phone[name])
+        else:
+            print("no number")
+    elif command == "2":
+        name = input("name: ")
+        numbers = (input("number: "))
+        phone[name] = numbers
+        print("ok!")
+    else:
+        print("quitting...")
+        break
