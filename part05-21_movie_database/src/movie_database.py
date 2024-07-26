3b681fdd24134bd8ff0859f9b7e0b2035f7c4457
ameliacitradewi <ameliacitradewi@gmail.com>
2024-07-26 21:57:33 +0700

    commit

--- part05-21_movie_database/src/movie_database.py
+++ part05-21_movie_database/src/movie_database.py
@@ -1,1 +1,4 @@
 # Write your solution here
+def add_movie(database: list, name: str, director: str, year: int, runtime: int):
+    movie_dict = {"name": name, "director": director, "year": year, "runtime": runtime}
+    database.append(movie_dict)
