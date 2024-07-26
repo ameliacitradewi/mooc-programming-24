08c935d5f25cbc325dbf9f0e950e091293fe1648
ameliacitradewi <ameliacitradewi@gmail.com>
2024-07-26 22:15:24 +0700

    commit

--- part05-22_find_movies/src/find_movies.py
+++ part05-22_find_movies/src/find_movies.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def find_movies(database: list, search_term: str):
+    search_term_lower = search_term.lower()
+    matching_movies = []
+    
+    for movie in database:
+        movie_name_lower = movie["name"].lower()
+        if search_term_lower in movie_name_lower:
+            matching_movies.append(movie)
+  
+    return matching_movies
