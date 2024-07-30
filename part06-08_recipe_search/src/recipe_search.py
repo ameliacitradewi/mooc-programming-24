f6cf7b20f1f2915f5de0b8dda4c25168b2a0f381
ameliacitradewi <ameliacitradewi@gmail.com>
2024-07-30 21:49:45 +0700

    commit

--- part06-08_recipe_search/src/recipe_search.py
+++ part06-08_recipe_search/src/recipe_search.py
@@ -1,1 +1,34 @@
 # Write your solution here
+def search_by_name(filename: str, word: str):
+    with open(filename) as file:
+        lines = file.readlines()
+        
+    word = word.lower()
+    recipes = []
+    current_recipe = []
+  
+    for line in lines:
+        line = line.strip()
+        if line == "":
+            if current_recipe:
+                recipes.append(current_recipe)
+                current_recipe = []
+        else:
+            current_recipe.append(line)
+    
+    if current_recipe:
+        recipes.append(current_recipe)
+    
+    found_recipes = []
+    for recipe in recipes:
+        recipe_name = recipe[0]
+        if word in recipe_name.lower():
+            found_recipes.append(recipe_name)
+    
+    return found_recipes
+
+# usage
+found_recipes = search_by_name("recipes1.txt", "cake")
+
+for recipe in found_recipes:
+    print(recipe)
