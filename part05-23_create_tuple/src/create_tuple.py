eea0f270ebb258a69fd9d2d64c3d9d275f526971
ameliacitradewi <ameliacitradewi@gmail.com>
2024-07-30 20:42:34 +0700

    commit

--- part05-23_create_tuple/src/create_tuple.py
+++ part05-23_create_tuple/src/create_tuple.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def create_tuple(x: int, y: int, z: int):
+    maximum = max(x, y, z)
+    minimum = min(x, y, z)
+    sum = x + y + z
+    final = (minimum, maximum, sum)
+    return final
+
+if __name__ == "__main__":
+    print(create_tuple(1, 4, 2))
+
+# The first element is the smallest of the arguments
+# The second element is the greatest of the arguments
+# The third element is the sum of the arguments
