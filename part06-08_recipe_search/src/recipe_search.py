fe5d9e53df4de829bace38db9b727332859f0a55
ameliacitradewi <ameliacitradewi@gmail.com>
2024-07-31 13:47:21 +0700

    commit

--- part06-08_recipe_search/src/recipe_search.py
+++ part06-08_recipe_search/src/recipe_search.py
@@ -1,34 +1,46 @@
-# Write your solution here
-def search_by_name(filename: str, word: str):
+# write your solution here
+def read_recipes(filename):
     with open(filename) as file:
         lines = file.readlines()
         
-    word = word.lower()
     recipes = []
-    current_recipe = []
+    current_recipe = {}
+    ingredients = []
   
     for line in lines:
         line = line.strip()
-        if line == "":
+        if not line:
             if current_recipe:
+                current_recipe['ingredients'] = ingredients
                 recipes.append(current_recipe)
-                current_recipe = []
+                current_recipe = {}
+                ingredients = []
+        elif 'name' not in current_recipe:
+            current_recipe['name'] = line
+        elif 'prep_time' not in current_recipe:
+            current_recipe['prep_time'] = int(line)
         else:
-            current_recipe.append(line)
+            ingredients.append(line)
     
     if current_recipe:
+        current_recipe['ingredients'] = ingredients
         recipes.append(current_recipe)
     
-    found_recipes = []
-    for recipe in recipes:
-        recipe_name = recipe[0]
-        if word in recipe_name.lower():
-            found_recipes.append(recipe_name)
-    
+    return recipes
+
+def search_by_name(filename, word):
+    recipes = read_recipes(filename)
+    word = word.lower()
+    found_recipes = [recipe['name'] for recipe in recipes if word in recipe['name'].lower()]
     return found_recipes
 
-# usage
-found_recipes = search_by_name("recipes1.txt", "cake")
+def search_by_time(filename, prep_time):
+    recipes = read_recipes(filename)
+    found_recipes = [f"{recipe['name']}, preparation time {recipe['prep_time']} min" for recipe in recipes if recipe['prep_time'] <= prep_time]
+    return found_recipes
 
-for recipe in found_recipes:
-    print(recipe)
+def search_by_ingredient(filename, ingredient):
+    recipes = read_recipes(filename)
+    ingredient = ingredient.lower()
+    found_recipes = [f"{recipe['name']}, preparation time {recipe['prep_time']} min" for recipe in recipes if any(ingredient in ing.lower() for ing in recipe['ingredients'])]
+    return found_recipes
