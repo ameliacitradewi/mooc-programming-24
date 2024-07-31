e8492c4285e1a793ffd2ecb0f69384d3f4577ab9
ameliacitradewi <ameliacitradewi@gmail.com>
2024-07-31 13:38:51 +0700

    commit

--- part06-18_parameter_validation/src/parameter_validation.py
+++ part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,9 @@
 # Write your solution here
+# def new_person(name: str, age: int):
+#     if name == "":
+#         raise ValueError("name is an empty string")
+#     elif len(name) > 40:
+#         raise ValueError("name is longer than 40 characters")
+#     elif " " not in name:
+#         raise ValueError("name containes less than two words")
+#     elif
