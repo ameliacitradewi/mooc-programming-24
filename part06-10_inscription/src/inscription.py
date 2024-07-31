2af9590120825ce0424f8ea882083e1c7d99b1e1
ameliacitradewi <ameliacitradewi@gmail.com>
2024-07-31 14:16:04 +0700

    commit

--- part06-10_inscription/src/inscription.py
+++ part06-10_inscription/src/inscription.py
@@ -1,1 +1,6 @@
 # Write your solution here
+name = input()
+name_file = input()
+
+with open(name_file, 'w') as file:
+    file.write(f"Hi {name}, we hope you enjoy learning Python with us! Best, Mooc.fi Team")
