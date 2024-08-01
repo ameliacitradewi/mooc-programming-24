7bb2b5ec59cb50facdaaca065b2750c3f6750195
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-01 19:35:58 +0700

    commit

--- part06-13_store_personal_data/src/store_personal_data.py
+++ part06-13_store_personal_data/src/store_personal_data.py
@@ -2,6 +2,5 @@
 def store_personal_data(person: tuple):
     with open("people.csv", 'w') as file:
         name, age, height = person[0], int(person[1]), float(person[2])
-        print(name, age, height)
         line = f"{name};{age};{height}"
         file.write(line + "\n")
--- part06-14_course_grading_part_4/src/course_grading_part_4.py
+++ part06-14_course_grading_part_4/src/course_grading_part_4.py
@@ -2,10 +2,19 @@
 file_name1 = input("Student information: ")
 file_name2 = input("Exercises completed: ")
 file_name3 = input("Exam final_point: ")
+header_file = input("Course information: ")
 # file_name1 = "students1.csv"
 # file_name2 = "exercises1.csv"
 # file_name3 = "exam_points1.csv"
+# header_file = "course1.txt"
 
+course_name = []
+with open(header_file) as file:
+    for line in file:
+        line = line.replace("\n", "")
+        parts = line.split(": ")
+        course_name.append(parts[1])
+    
 students = {}
 with open(file_name1) as file:
     for line in file:
@@ -33,30 +42,36 @@ with open(file_name3) as file:
         if parts[0] == "id":
             continue
         exam[parts[0]] = sum(map(int, parts[1:4]))
-        
 
-print(f"{'name':30}{'exec_nbr':<10}{'exec_pts.':<10}{'exm_pts.':<10}{'tot_pts.':<10}{'grade':<10}")
 
-for id, name in students.items():
-    if id in exercise:
-        exercise_complete = exercise[id]
-        exam_complete = exam[id]
-        calculate = exercise_complete // 4
-        final_point = exam_complete + calculate
-        grade = 0
-        
-        if 0 <= final_point <= 14:
-            grade = 0  # Fail
-        elif 15 <= final_point <= 17:
-            grade =  1
-        elif 18 <= final_point <= 20:
-            grade = 2
-        elif 21 <= final_point <= 23:
-            grade = 3
-        elif 24 <= final_point <= 27:
-            grade =  4
-        else:
-            grade = 5
-        print(f"{name:30}{exercise_complete:<10}{calculate:<10}{exam_complete:<10}{final_point:<10}{grade:<10}")
-    else:
-        print("Not found.")
+with open("results.txt", "w") as results_file:
+    results_file.write(f"{course_name[0]}, {course_name[1]} credits\n")
+    results_file.write("=" * 38 + "\n")
+    results_file.write(f"{'name':30}{'exec_nbr':<10}{'exec_pts.':<10}{'exm_pts.':<10}{'tot_pts.':<10}{'grade':<10}\n")
+
+    with open("results.csv", 'w') as csv_file:
+
+        for id, name in students.items():
+            exercise_complete = exercise.get(id, 0)
+            exam_complete = exam.get(id, 0)
+            calculate = exercise_complete // 4
+            final_point = exam_complete + calculate
+            grade = 0
+
+            if 0 <= final_point <= 14:
+                grade = 0  # Fail
+            elif 15 <= final_point <= 17:
+                grade = 1
+            elif 18 <= final_point <= 20:
+                grade = 2
+            elif 21 <= final_point <= 23:
+                grade = 3
+            elif 24 <= final_point <= 27:
+                grade = 4
+            else:
+                grade = 5
+
+            results_file.write(f"{name:30}{exercise_complete:<10}{calculate:<10}{exam_complete:<10}{final_point:<10}{grade:<10}\n")
+            csv_file.write(f"{id};{name};{grade}\n")
+
+print("Results written to files results.txt and results.csv")
