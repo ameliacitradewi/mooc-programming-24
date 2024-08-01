b69c2512b8fb2eb0bd504e9c00a56e9228e6a452
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-01 18:17:33 +0700

    commit

--- part06-13_store_personal_data/src/store_personal_data.py
+++ part06-13_store_personal_data/src/store_personal_data.py
@@ -1,1 +1,7 @@
 # Write your solution here
+def store_personal_data(person: tuple):
+    with open("people.csv", 'w') as file:
+        name, age, height = person[0], int(person[1]), float(person[2])
+        print(name, age, height)
+        line = f"{name};{age};{height}"
+        file.write(line + "\n")
