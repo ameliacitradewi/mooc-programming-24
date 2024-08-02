165e653e209f48937390737859c2c6989441450e
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-02 17:13:30 +0700

    commit

--- part07-04_lottery_numbers/src/lottery_numbers.py
+++ part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,7 @@
 # Write your solution here
+import random
+
+def lottery_numbers(amount: int, lower: int, upper: int):
+    numbers = random.sample(range(lower, upper + 1), amount)
+    numbers.sort()
+    return numbers
