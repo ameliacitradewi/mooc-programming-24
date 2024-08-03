173b3a4fe11a396aa9685f46ce54558ec6ba5c8e
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-03 23:54:20 +0700

    commit

--- part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,1 +1,14 @@
 # Write your solution here
+import random as r
+import string as s
+
+def generate_password(length: int):
+    letters = s.ascii_lowercase
+    password = ""
+    for char in range(length):
+        password += r.choice(letters)
+    return password
+
+# # Example Usage
+# for i in range(5):
+#   print(generate_password(3))
