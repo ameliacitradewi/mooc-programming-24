833b1820282fd1e89cdbf39d46d1b039b5ab5d68
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-06 23:31:39 +0700

    commit

--- part07-08_random_words/src/random_words.py
+++ part07-08_random_words/src/random_words.py
@@ -2,14 +2,11 @@
 import random
 
 def words(n: int, beginning: str) -> list:
-    # Read the words from the file
     with open('words.txt', 'r') as file:
         all_words = file.read().splitlines()
     
-    # Filter words that start with the specified beginning string
     filtered_words = [word for word in all_words if word.startswith(beginning)]
     
-    # Randomly select n unique words from the filtered list
     selected_words = random.sample(filtered_words, n)
     
     return selected_words
