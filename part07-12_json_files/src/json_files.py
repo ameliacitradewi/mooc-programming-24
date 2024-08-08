b0b29e9bd275cc636e827c416fc99977527343a7
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-08 23:46:09 +0700

    commit

--- part07-12_json_files/src/json_files.py
+++ part07-12_json_files/src/json_files.py
@@ -1,1 +1,11 @@
 # Write your solution here
+import json
+
+def print_persons(filename: str):
+  with open(filename, 'r') as file:
+      data = json.load(file)
+
+  for person in data:
+      name, age = person['name'], person['age']
+      hobbies = ', '.join(person['hobbies'])
+      print(f"{name} {age} years ({hobbies})")
