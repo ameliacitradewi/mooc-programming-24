d93941defb4a6e38f7998216af38afe285652c77
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-12 20:47:03 +0700

    commit

--- part07-16_spellchecker_2/src/spellchecker_2.py
+++ part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,32 @@
-# Write your solution here
+# write your solution here
+import difflib
+
+text = input("write text: ")
+wordlist = []  # The complete word from wordlist.txt
+
+check = text.split()
+checked_text = []  # save the checked word as list
+misspelled = []  # save the misspelled word for suggestions
+
+with open("wordlist.txt") as file:
+    for line in file:
+        line = line.replace("\n", "")
+        word = line.strip().lower()
+        wordlist.append(word)
+
+for char in check:
+    if char.lower() not in wordlist:
+        misspelled.append(char.lower())
+        checked_text.append(f"*{char}*")
+    else:
+        checked_text.append(char)
+
+print(" ".join(checked_text))
+print("suggestions:")
+
+for words in misspelled:
+    suggestions = difflib.get_close_matches(words, wordlist)
+    print(f"{words}: {', '.join(suggestions)}")
+
+
+
