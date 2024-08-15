78a382910d8d1e3f14e9116aec5744a51d446fa5
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-15 21:57:28 +0700

    commit

--- part08-10_decreasing_counter/src/decreasing_counter.py
+++ part08-10_decreasing_counter/src/decreasing_counter.py
@@ -1,12 +1,21 @@
-# Tee ratkaisusi tähän:
+# Write your solution here
 class DecreasingCounter:
     def __init__(self, initial_value: int):
         self.value = initial_value
+        self.reset = initial_value
 
     def print_value(self):
         print("value:", self.value)
 
     def decrease(self):
-        pass
+        if self.value > 0:
+            self.value -= 1
+        else:
+            self.value == 0
 
     # Write the rest of the methods here!
+    def set_to_zero(self):
+        self.value = 0
+
+    def reset_original_value(self):
+        self.value = self.reset
