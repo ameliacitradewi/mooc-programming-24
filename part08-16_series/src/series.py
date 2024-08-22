560492c363d2c9d9fa0a0928473ea158fa0804aa
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-22 12:17:38 +0700

    commit

--- part08-16_series/src/series.py
+++ part08-16_series/src/series.py
@@ -9,7 +9,7 @@ class Series:
     def rate(self, rating: int):
         if 0 <= rating <= 5:
             self.ratings.append(rating)
-    
+
     def __str__(self):
         genre_string = ", ".join(self.genres)
         if self.ratings:
@@ -22,11 +22,28 @@ class Series:
                   f"genres: {genre_string}\n"
                   "no ratings")
     
+def minimum_grade(rating: float, series_list: list):
+    return [series for series in series_list if series.ratings and (sum(series.ratings) / len(series.ratings)) >= rating]
+    
+def includes_genre(genre: str, series_list: list):
+    return [series for series in series_list if genre in series.genres]
+     
 #Example Usage
-dexter = Series("Dexter", 8, ["Crime", "Drama", "Mystery", "Thriller"])
-dexter.rate(4)
-dexter.rate(5)
-dexter.rate(5)
-dexter.rate(3)
-dexter.rate(0)
-print(dexter)
+# s1 = Series("Dexter", 8, ["Crime", "Drama", "Mystery", "Thriller"])
+# s1.rate(5)
+
+# s2 = Series("South Park", 24, ["Animation", "Comedy"])
+# s2.rate(3)
+
+# s3 = Series("Friends", 10, ["Romance", "Comedy"])
+# s3.rate(2)
+
+# series_list = [s1, s2, s3]
+
+# print("a minimum grade of 4.5:")
+# for series in minimum_grade(4.5, series_list):
+#     print(series.title)
+
+# print("genre Comedy:")
+# for series in includes_genre("Comedy", series_list):
+#     print(series.title)
