9b454e332e8f0f227c6a0049d34786979793ee98
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-26 21:11:42 +0700

    commit

--- part09-07_box_of_presents/src/box_of_presents.py
+++ part09-07_box_of_presents/src/box_of_presents.py
@@ -1,1 +1,32 @@
 # WRITE YOUR SOLUTION HERE:
+class Present:
+    def __init__(self, name: str, weight: int):
+        self.name = name
+        self.weight = weight
+    
+    def __str__(self):
+        return f"{self.name} ({self.weight} kg)"
+
+class Box:
+    def __init__(self):
+        self.present = []
+
+    def add_present(self, present: Present):
+        self.present.append(present)
+
+    def total_weight(self):
+        weight = []
+        for presents in self.present:
+            weight.append(presents.weight)
+        return sum(weight)
+
+# Example Usage
+# book = Present("ABC Book", 2)
+
+# box = Box()
+# box.add_present(book)
+# print(box.total_weight())
+
+# cd = Present("Pink Floyd: Dark Side of the Moon", 1)
+# box.add_present(cd)
+# print(box.total_weight())
