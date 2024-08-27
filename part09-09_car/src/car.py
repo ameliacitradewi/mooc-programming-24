dbd5029c58e77c2d360cad24f7459c80335a7367
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-27 21:31:40 +0700

    commit

--- part09-09_car/src/car.py
+++ part09-09_car/src/car.py
@@ -1,1 +1,34 @@
 # WRITE YOUR SOLUTION HERE:
+class Car:
+    def __init__(self):
+        self.__tank = 0
+        self.__odo = 0  
+    
+    def fill_up(self):
+        self.__tank = 60
+    
+    def drive(self, km: int):
+        if self.__tank > km:
+            self.__odo += km
+            self.__tank -= km
+        else:
+            self.__odo += self.__tank
+            self.__tank = 0
+    
+    def __str__(self):
+        return f"Car: odometer reading {self.__odo} km, petrol remaining {self.__tank} litres"
+    
+# Example Usage
+car = Car()
+print(car)
+car.fill_up()
+print(car)
+car.drive(20)
+print(car)
+car.drive(50)
+print(car)
+car.drive(10)
+print(car)
+car.fill_up()
+car.fill_up()
+print(car)
