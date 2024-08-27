18744d417a91bce27cac28c4cbe024bd15eb2f17
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-27 22:17:26 +0700

    commit

--- part09-10_recording/src/recording.py
+++ part09-10_recording/src/recording.py
@@ -1,1 +1,21 @@
 # WRITE YOUR SOLUTION HERE:
+class Recording:
+    def __init__(self, length: int):
+        self.__length = length
+    
+    @property
+    def length(self):
+        return self.__length
+
+    @length.setter
+    def length(self, length):
+        if length > 0:
+            self.__length = length
+        else:
+            raise ValueError("The amount must be bigger than zero")
+
+# Example Usage
+the_wall = Recording(43)
+print(the_wall.length)
+the_wall.length = 44
+print(the_wall.length)
