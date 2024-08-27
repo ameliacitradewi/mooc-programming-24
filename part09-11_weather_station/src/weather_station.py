8591df66ae6cd79d4dff7bbb205d202e42f7d5f8
ameliacitradewi <ameliacitradewi@gmail.com>
2024-08-27 22:44:51 +0700

    commit

--- part09-11_weather_station/src/weather_station.py
+++ part09-11_weather_station/src/weather_station.py
@@ -19,16 +19,16 @@ class WeatherStation:
       return f"{self._name}, {self.number_of_observations()} observations"
 
 # Example usage
-station = WeatherStation("Houston")
-station.add_observation("Rain 10mm")
-station.add_observation("Sunny")
-print(station.latest_observation())
+# station = WeatherStation("Houston")
+# station.add_observation("Rain 10mm")
+# station.add_observation("Sunny")
+# print(station.latest_observation())  # Output: Sunny
 
-station.add_observation("Thunderstorm")
-print(station.latest_observation())
+# station.add_observation("Thunderstorm")
+# print(station.latest_observation())  # Output: Thunderstorm
 
-print(station.number_of_observations())
-print(station)
+# print(station.number_of_observations())  # Output: 3
+# print(station)  # Output: Houston, 3 observations
 
     
 
