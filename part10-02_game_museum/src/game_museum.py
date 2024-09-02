0487c581174a52ac072bb7d8d58949aa7d2f8e81
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-02 20:37:39 +0700

    commit

--- part10-02_game_museum/src/game_museum.py
+++ part10-02_game_museum/src/game_museum.py
@@ -15,3 +15,10 @@ class GameWarehouse:
     def list_games(self):
         return self.__games
 
+class GameMuseum(GameWarehouse):
+    def __init__(self):
+        super().__init__()
+
+    def list_games(self):
+        all_games = super().list_games()
+        return [game for game in all_games if game.year < 1990]
