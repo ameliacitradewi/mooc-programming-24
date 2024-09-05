dffab078c8be4eb29d644796db7f31cd1db7b6af
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-05 23:21:00 +0700

    commit

--- part10-09_iterable_shopping_list/src/iterable_shopping_list.py
+++ part10-09_iterable_shopping_list/src/iterable_shopping_list.py
@@ -26,3 +26,12 @@ class ShoppingList:
             return product
         else:
             raise StopIteration
+
+# Example Usage
+shopping_list = ShoppingList()
+shopping_list.add("bananas", 10)
+shopping_list.add("apples", 5)
+shopping_list.add("pineapple", 1)
+
+for product in shopping_list:
+    print(f"{product[0]}: {product[1]} units")
