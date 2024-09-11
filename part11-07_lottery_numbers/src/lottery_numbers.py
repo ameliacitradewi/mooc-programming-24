e5ecd4bedf09d21505049ff2bb4fbc7105be922d
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-12 00:07:20 +0700

    commit

--- part11-07_lottery_numbers/src/lottery_numbers.py
+++ part11-07_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,11 @@
 # WRITE YOUR SOLUTION HERE:
+class LotteryNumbers:
+    def __init__(self, week: int, correct_numbers: list):
+        self.week = week
+        self.correct_numbers = correct_numbers
+
+    def number_of_hits(self, numbers: list):
+        return len([num for num in numbers if num in self.correct_numbers])
+    
+    def hits_in_place(self, numbers: list):
+        return [num if num in self.correct_numbers else -1 for num in numbers]
