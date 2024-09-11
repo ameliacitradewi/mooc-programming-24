d1bc1b784cb4e93ae1bb8411eb3418e34e5948b4
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-12 00:35:01 +0700

    commit

--- part11-11_lengths_of_strings/src/lengths_of_strings.py
+++ part11-11_lengths_of_strings/src/lengths_of_strings.py
@@ -1,1 +1,10 @@
 # WRITE YOUR SOLUTION HERE:
+def lengths(strings: list):
+    return {string : len(string) for string in strings}
+
+
+# Example Usage
+word_list = ["once", "upon" , "a", "time", "in"]
+
+word_lengths = lengths(word_list)
+print(word_lengths)
