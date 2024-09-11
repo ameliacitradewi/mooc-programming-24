dd713278d43c84df367328b5a78e619e2a4f5e3a
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-12 00:58:58 +0700

    commit

--- part11-12_most_common_words/src/most_common_words.py
+++ part11-12_most_common_words/src/most_common_words.py
@@ -1,1 +1,15 @@
 # WRITE YOUR SOLUTION HERE:
+import string
+from collections import Counter
+
+def most_common_words(filename: str, lower_limit: int):
+    with open(filename, 'r') as file:
+        text = file.read()
+
+    text = text.translate(str.maketrans('','', string.punctuation))
+    words_counts = Counter(text.split())
+
+    return {word: count for word, count in words_counts.items() if count >= lower_limit}
+
+# Example Usage
+print(most_common_words("comprehensions.txt", 3))
