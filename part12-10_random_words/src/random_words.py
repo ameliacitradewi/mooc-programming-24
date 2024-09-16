31dba3e114b151a92d17fdbfa8b74affb4df7300
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-17 00:21:40 +0700

    commit

--- part12-10_random_words/src/random_words.py
+++ part12-10_random_words/src/random_words.py
@@ -1,1 +1,5 @@
 # Write your solution here:
+import random
+
+def word_generator(characters: str, length: int, amount: int):
+    return (''.join(random.choice(characters) for _ in range(length)) for _ in range(amount))
