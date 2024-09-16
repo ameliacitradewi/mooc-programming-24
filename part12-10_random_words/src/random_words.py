2a198878a7b83fa968ad3704ef4aed70957e6cf7
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-17 00:24:21 +0700

    commit

--- part12-10_random_words/src/random_words.py
+++ part12-10_random_words/src/random_words.py
@@ -3,3 +3,8 @@ import random
 
 def word_generator(characters: str, length: int, amount: int):
     return (''.join(random.choice(characters) for _ in range(length)) for _ in range(amount))
+
+# Example Usage
+wordgen = word_generator("abcdefg", 3, 5)
+for word in wordgen:
+    print(word)
