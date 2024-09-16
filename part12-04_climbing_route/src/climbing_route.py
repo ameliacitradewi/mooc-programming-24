6b2563cfa41b959d1d7f5471b5f12a8a28d5bb71
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-16 22:14:38 +0700

    commit

--- part12-04_climbing_route/src/climbing_route.py
+++ part12-04_climbing_route/src/climbing_route.py
@@ -8,3 +8,23 @@ class ClimbingRoute:
         return f"{self.name}, length {self.length} metres, grade {self.grade}"
 
 # Write your solution here:
+def sort_by_length(routes: list):
+    def order_by_length(route: tuple):
+        return route.length
+    return sorted(routes, key=order_by_length, reverse=True)
+
+def sort_by_difficulty(routes: list):
+    diff_routes = sort_by_length(routes)
+    def order_by_difficulty(diff_routes: tuple):
+        return diff_routes.grade
+    return sorted(diff_routes, key=order_by_difficulty, reverse=True)
+
+# Example Case
+r1 = ClimbingRoute("Edge", 38, "6A+")
+r2 = ClimbingRoute("Smooth operator", 11, "7A")
+r3 = ClimbingRoute("Synchro", 14, "8C+")
+r4 = ClimbingRoute("Small steps", 12, "6A+")
+
+routes = [r1, r2, r3, r4]
+for route in sort_by_difficulty(routes):
+    print(route)
