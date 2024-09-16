3b6547dbf18e4c1ba443c021bf15b30518e1fd60
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-17 00:10:28 +0700

    commit

--- part12-09_prime_numbers/src/prime_numbers.py
+++ part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def prime_numbers():
+    numbers = 2
+    while True:
+        if all(numbers % i != 0 for i in range(2, int(numbers**0.5) + 1)):
+            yield numbers
+        numbers += 1
+
+# Example Usage
+numbers = prime_numbers()
+for i in range(8):
+    print(next(numbers))
