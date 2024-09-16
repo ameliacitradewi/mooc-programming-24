d96453b6d34592dc9a87d9b156d2735808e274ba
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-16 18:50:36 +0700

    commit

--- part12-02_seasons/src/seasons.py
+++ part12-02_seasons/src/seasons.py
@@ -1,1 +1,12 @@
 # Write your solution here:
+def sort_by_seasons(items: list):
+    def order_by_seasons(item: dict):
+        return item.get('seasons')
+    
+    return sorted(items, key= order_by_seasons)
+
+# Example Test
+shows = [{ "name": "Dexter", "rating" : 8.6, "seasons":9 }, { "name": "Friends", "rating" : 8.9, "seasons":10 },  { "name": "Simpsons", "rating" : 8.7, "seasons":32 }  ]
+
+for show in sort_by_seasons(shows):
+    print(f"{show['name']} {show['seasons']} seasons")
