0de7fce897f136294151232a2105e7ff91e4137e
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-17 00:03:43 +0700

    commit

--- part12-08_even_numbers/src/even_numbers.py
+++ part12-08_even_numbers/src/even_numbers.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def even_numbers(beginning: int, maximum: int):
+    number = beginning
+    while number <= maximum:
+        if number % 2 == 0:
+            yield number
+        number += 1
+
+# Example Usage
+numbers = even_numbers(11, 21)
+for number in numbers:
+    print(number)
