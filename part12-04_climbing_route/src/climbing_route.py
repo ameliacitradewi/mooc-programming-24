d61d15f813e858604a196a45bbc35f05b1cd91fd
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-16 19:52:56 +0700

    commit

--- part12-04_climbing_route/src/climbing_route.py
+++ part12-04_climbing_route/src/climbing_route.py
@@ -7,4 +7,17 @@ class ClimbingRoute:
     def __str__(self):
         return f"{self.name}, length {self.length} metres, grade {self.grade}"
 
-# Write your solution herer:
+# Write your solution here:
+def sort_by_length(routes: [ClimbingRoute]) -> list:
+    return sorted(routes, key=lambda route: route.length, reverse=True)
+
+
+r1 = ClimbingRoute("Edge", 38, "6A+")
+r2 = ClimbingRoute("Smooth operator", 11, "7A")
+r3 = ClimbingRoute("Synchro", 14, "8C+")
+r4 = ClimbingRoute("Small steps", 12, "6A+")
+
+routes = [r1, r2, r3, r4]
+
+for route in sort_by_length(routes):
+    print(route)
