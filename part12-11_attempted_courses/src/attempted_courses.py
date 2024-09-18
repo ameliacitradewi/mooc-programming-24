ae3c16c7173129870b1038dab386a11928b04f0d
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-18 20:19:52 +0700

    commit

--- part12-11_attempted_courses/src/attempted_courses.py
+++ part12-11_attempted_courses/src/attempted_courses.py
@@ -8,3 +8,18 @@ class CourseAttempt:
         return f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
 
 # Write your solution here
+def names_of_students(attempts: list):
+    return map(lambda x: x.student_name, attempts)
+
+def course_names(attempts: list):
+    return set(map(lambda x: x.course_name, attempts))
+
+# Example Usage
+s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 2)
+
+for name in names_of_students([s1, s2, s3]):
+    print(name)
+for name in course_names([s1, s2, s3]):
+    print(name)
