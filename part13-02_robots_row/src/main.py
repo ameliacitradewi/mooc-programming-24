f704a1956f43239af3af09999769fb184636f08a
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-20 00:03:29 +0700

    commit

--- part13-02_robots_row/src/main.py
+++ part13-02_robots_row/src/main.py
@@ -1,1 +1,23 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+width = robot.get_width()
+height = robot.get_height()
+
+window.fill((0,0,0))
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+number_of_robot = 10
+for i in number_of_robot:
+    window.blit(robot, (32, width))
+    width += width
+
