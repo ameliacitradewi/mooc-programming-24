aa7135040920b095493564c00aa7ca9b63adc268
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-20 00:32:58 +0700

    commit

--- part13-07_two_robots/src/main.py
+++ part13-07_two_robots/src/main.py
@@ -1,1 +1,30 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot1 = pygame.image.load("robot.png")
+
+x = 0
+y = 0
+velocity = 1
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    x += velocity
+    if velocity > 0 and x+robot.get_width() >= 640:
+        velocity = -velocity
+    if velocity < 0 and x <= 0:
+        velocity = -velocity
+
+    window.fill((0, 0, 0))
+    window.blit(robot1, (x, y))
+    window.blit(robot2, ((x * 2), (y * 2)))
+    pygame.display.flip()
+
+    clock.tick(60)
