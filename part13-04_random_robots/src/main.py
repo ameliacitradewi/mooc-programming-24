cc164f966dd0996db08dd71448299d7a282aff40
ameliacitradewi <ameliacitradewi@gmail.com>
2024-09-20 00:08:18 +0700

    commit

--- part13-04_random_robots/src/main.py
+++ part13-04_random_robots/src/main.py
@@ -1,1 +1,20 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame
+import random
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+window.fill((0,0,0))
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+total_robot = 1000
+for i in total_robot:
+    window.blit(robot, (random.randint(0, 480), random.randint(0, 640)))
